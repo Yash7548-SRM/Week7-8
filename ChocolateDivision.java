bac9baf975a795aa81abad24772bfcb8c25c91b2
Yash Ahlawat <ya7548@srmist.edu.in>
2026-04-05 22:13:21 +0530

    Q9

---
+++ ChocolateDivision.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ChocolateDivision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chocolates = sc.nextInt();
+        int children = sc.nextInt();
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println("Chocolates per child: " + result[0] + ", Remaining: " + result[1]);
+    }
+
+    public static int[] findRemainderAndQuotient(int number, int divisor) {
+        return new int[]{number / divisor, number % divisor};
+    }
+}
